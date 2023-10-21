289d7c9c62277ae34a4bf957a215177f78f76623
tanvir <tanvirulislam149@gmail.com>
2023-10-22 00:27:37 +0600

    pie chart edited

--- Components/PieChart.js
+++ Components/PieChart.js
@@ -5,36 +5,50 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart = ({ data }) => {
-  const finalData = data.slice(0, 25);
-  let country = finalData.map(a => a.country);
-  let region = finalData.map(a => a.region);
+  const finalData = data.slice(0, 299); // taking 300 elements of the array
+  const unique = finalData.filter((obj, index) => {  // removing elements that have the same region
+    return index === finalData.findIndex(o => obj.region === o.region);
+  });
+  let region = unique.map(a => a.region); // making an array of all unique region
+  let borderArray = [];
+
+  const colorArray = unique.map(d => {  // making random color array according to the "unique" array
+    const r = Math.floor(Math.random() * (255 + 1));
+    const g = Math.floor(Math.random() * (255 + 1));
+    const b = Math.floor(Math.random() * (255 + 1));
+    borderArray.push(`rgb(${r}, ${g}, ${b})`);
+    const color = `rgba(${r}, ${g}, ${b}, 0.2)`;
+    return color;
+  })
+
+
+
   const options = {
     parsing: {
       key: "intensity"
-    }
+    },
+    plugins: {
+      legend: {
+        position: 'left',
+      },
+    },
   };
   const data2 = {
     labels: region,
     datasets: [
       {
         label: 'Intensity',
-        data: finalData,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-        ],
+        data: unique,
+        backgroundColor: colorArray,
+        borderColor: borderArray,
         borderWidth: 1,
       },
     ],
   }
+
+
   return (
-    <div className='w-4/5 h-4/5 mx-auto border-2 border-neutral-300 rounded-lg bg-white p-10'>
+    <div className='w-8/12 mx-auto border-2 border-neutral-300 rounded-lg bg-white p-10'>
       <p className='text-2xl pb-5 font-medium'>Pie Chart</p>
       <Pie
         options={options}
