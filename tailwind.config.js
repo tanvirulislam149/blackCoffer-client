c1221e5223040fea751067d657b1623c1ffeb246
tanvir <tanvirulislam149@gmail.com>
2023-10-23 02:01:22 +0600

    issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
